369e8df226dfce5f5369ff813639379ded9a2268
Lukas Schlötterer <80917404+lschloetterer@users.noreply.github.com>
2026-04-08 19:39:56 +0200

    delete removed lists from pihole

--- nixos/modules/services/networking/pihole-ftl-setup-script.nix
+++ nixos/modules/services/networking/pihole-ftl-setup-script.nix
@@ -9,12 +9,13 @@ let
   pihole = cfg.piholePackage;
   makePayload =
     list:
-    builtins.toJSON {
+    {
       inherit (list) type enabled;
       address = list.url;
       comment = list.description;
     };
   payloads = map makePayload cfg.lists;
+  expectedListsJSON = builtins.toJSON payloads;
   macvendorURL = lib.strings.escapeShellArg cfg.macvendorURL;
 in
 ''
@@ -36,6 +37,7 @@ in
   source ${pihole}/share/pihole/advanced/Scripts/utils.sh
 
   any_failed=0
+  any_changed=0
 
   addList() {
     local payload="$1"
@@ -59,6 +61,24 @@ in
     fi
 
     echo "Added list ID $id: $result"
+    any_changed=1
+  }
+
+  removeListsBatch() {
+    local payload="$1"
+
+    echo "Removing lists: $payload"
+    local result=$(PostFTLData "lists:batchDelete" "$payload")
+
+    local error="$($jq '.error' <<< "$result")"
+    if [[ "$error" != "" ]]; then
+        echo "Error: $error"
+        any_failed=1
+        return
+    fi
+
+    echo "Removed lists"
+    any_changed=1
   }
 
   for i in 1 2 3; do
@@ -69,17 +89,27 @@ in
 
   LoginAPI
 
-  ${builtins.concatStringsSep "\n" (
-    map (
-      payload:
-      lib.pipe payload [
-        lib.strings.escapeShellArg
-        (payload: "addList ${payload}")
-      ]
-    ) payloads
-  )}
+  expectedLists="${expectedListsJSON}"
+
+  # get lists currently on pihole
+  actualListsResponse=$(GetFTLData "lists")
+  actualLists=$($jq -r '.lists | pick(.[] | .type,.enabled,.address,.comment)' <<< "$actualListsResponse")
+
+  # get lists on pihole but not in $expectedLists
+  listsToRemove=$($jq -c -n --argjson expected "$expectedLists" --argjson actual "$actualLists" '{"expected": $expected,"actual":$actual} | .actual-.expected | [.[] | .["item"] = .address] | pick(.[] | .type,.item)')
+  if [[ $listsToRemove != "null" ]]; then
+    removeListsBatch "$listsToRemove"
+  fi
+
+  # iterate lists in $expectedLists but not on pihole
+  readarray -t listsToAdd < <($jq -c -n --argjson expected "$expectedLists" --argjson actual "$actualLists" '{"expected": $expected,"actual":$actual} | .expected-.actual | .[]')
+  for i in "''${listsToAdd[@]}"; do
+    addList "$i"
+  done
 
   # Run gravity.sh to load any new lists
-  $pihole -g
+  if [[ $any_changed -eq 1 ]]; then
+    $pihole -g
+  fi
   exit $any_failed
 ''
